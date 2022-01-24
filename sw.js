6381b568c97e0b8d9646c54cc5d7b9cc9da629fc
XMoSalahX <01154615199>
2022-01-24 07:39:06 +0200

    s

--- sw.js
+++ sw.js
@@ -36,24 +36,26 @@ self.addEventListener("activate", function(e) {
     )
 })
 
+var x = 0
 
 self.addEventListener('fetch', (e) => {
     e.respondWith((async() => {
-
-
-
+        if (x === 0) {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                x++
+                return r;
+            }
+        }
+        const m = await caches.match(e.request);
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName)
 
         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
         cache.put(e.request, response.clone());
 
-        const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) {
-            return r;
-        }
-        return response;
+        return response || m;
 
 
     })());
