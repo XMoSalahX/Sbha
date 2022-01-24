40a1b2c70bc6570a1dc612a73282228659343d84
XMoSalahX <01154615199>
2022-01-24 07:48:24 +0200

    test

--- sw.js
+++ sw.js
@@ -36,27 +36,35 @@ self.addEventListener("activate", function(e) {
     )
 })
 
-var x = 0
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith((async() => {
-        if (x === 0) {
-            const r = await caches.match(e.request);
-            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-            if (r) {
-                x++
-                return r;
-            }
-        }
-        const m = await caches.match(e.request);
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName)
-
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-
-        return response || m;
-
-
-    })());
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        }),
+    );
 });
+// var x = 0
+
+// self.addEventListener('fetch', (e) => {
+//     e.respondWith((async() => {
+//         if (x === 0) {
+//             const r = await caches.match(e.request);
+//             console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//             if (r) {
+//                 x++
+//                 return r;
+//             }
+//         }
+//         const m = await caches.match(e.request);
+//         const response = await fetch(e.request);
+//         const cache = await caches.open(cacheName)
+
+//         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//         cache.put(e.request, response.clone());
+
+//         return response || m;
+
+
+//     })());
+// });
